34fcf6d7d01b0a4ec88492ac0cf24c5a2fd02686
arunthegiri <arunthegiri@gmail.com>
2025-01-13 11:15:40 -0800

    Update Current_Price.py
    removing API key

--- Current_Price.py
+++ Current_Price.py
@@ -7,7 +7,7 @@ symbol = st.text_input("Enter a stock symbol", "AAPL")
 with st.sidebar:
     #Get your Polygon API key from the dashboard. 
     #In a real app you want to store this as a secret in your .env
-    polygon_api_key = st.text_input("pztqUHupdAm0s5oWz_puApQgHdMbV3pP", type="password")
+    polygon_api_key = st.text_input("", type="password")
     # Authenticate with the Polygon API
     client = RESTClient(polygon_api_key)
     col1, col2, col3 = st.columns(3)
